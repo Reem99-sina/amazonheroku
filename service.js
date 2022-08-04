af3b7c8d576f13f82c8e76261a5030cfd4021b92
Reem99-sina <reemsina2@gmail.com>
2022-08-04 23:06:32 +0200

    sowred

--- service.js
+++ service.js
@@ -6,8 +6,10 @@ import userrouter from './routers/sendRouter.js'
 import productRouter from './routers/productRouter.js'
 import userRouter from './routers/users.router.js'
 import orderRouter from './routers/order.router.js'
+import cors from 'cors'
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.get("/api/keys/paypal", (req, res) => {
@@ -17,7 +19,6 @@ app.use("/api/products", productRouter)
 app.use("/api/product", userrouter)
 app.use("/api/users", userRouter)
 app.use("/api/orders", orderRouter)
-
 app.use((err, req, res, next) => { res.status(500).json({ message: err.message }) })
 mongoose.connect(process.env.MONGODB_URL).then(() => { console.log('connect done') }).catch((error) => { console.log(error) })
 app.listen(process.env.PORT, () => {
